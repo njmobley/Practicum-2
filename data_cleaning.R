b6c445ce24060e9f015909e9204dfcd942ba1e1c
allimccarty <mccartar@iu.edu>
2021-03-21 10:55:59 -0400

    updates

--- data_cleaning.R
+++ data_cleaning.R
@@ -8,8 +8,8 @@ raw = read_csv("prac_data.csv")
 raw_data = read_csv("prac_data.csv") %>%
   mutate(
     price = unlist(read_csv("price.csv")),
+    job = dplyr::recode(job,unknown = "Unknown"),
     job = as.factor(replace_na(job,"Unknown")),
-    job = recode(job,unknown = "Unknown"),
     education = replace_na(education,"Unknown"),
     education = replace(education,which(education == "unknown"),"Unknown"),
     contact = replace_na(contact,"Unknown"),
@@ -37,7 +37,7 @@ imputedData = raw_data %>%
     age = replace_na(age,medianAge),
     duration = replace_na(duration,medianDuration),
     log_balance = log (balance - (-8019)),
-    job_reduced = recode(job,Unknown = "unknown",
+    job_reduced = dplyr::recode(job,Unknown = "unknown",
                                  student = "student",
                                  housemaid = "blue-collar",
                                  unemployed = "unemployed",
